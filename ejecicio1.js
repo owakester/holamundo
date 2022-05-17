f94c3c2cd96aff7b085621acb1830a27990d0801
Daishop <91887835+Daishop@users.noreply.github.com>
2022-05-16 21:08:43 -0500

    Ejercicio
    Ejercicio 1

---
+++ ejecicio1.js
@@ -0,0 +1,12 @@
+const autos = ["audi","mazda","chevrolet","picanto"]
+
+opcion = prompt("Digite el carro que desea comprar")
+
+if (autos.includes(opcion)) {
+    console.log("Lo tenemos¡¡")
+} else {
+    alert("No lo tenemos¡¡")
+}
+
+
+
